ded87596d0ed266232bb0dfb6f4d90997bc15e9e
Andy Walker <g00398788@gmit.ie>
2021-02-08 10:23:38 +0000

    Week 3

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ pands-problem-sheet
 Created pands-problem-sheet repository
 Uploaded program for Week 2 task - bmi.py
 This program calculates a person's BMI
+
+8/2/21
+Uploaded program for Week 3 task - secondString.py
+This program asks a user to input a string and outputs every second letter in reverse order. 
---
+++ secondString.py
@@ -0,0 +1,7 @@
+# secondString.py
+# A program that asks a user to input a string and outputs every second letter in reverse order. 
+# Author: Andy Walker
+
+# The program below produces result as per output shown on Weekly Task page (i.e. every second character)
+txt = input("Please enter a sentence: ")
+print (txt[::-2])
