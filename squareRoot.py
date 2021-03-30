6b87fcad69564c79a5185507c4cf2f136b4f1fa4
Andy Walker <g00398788@gmit.ie>
2021-03-30 12:31:51 +0100

    Change code to count iterations

--- squareRoot.py
+++ squareRoot.py
@@ -6,6 +6,7 @@
 # reference: https://medium.com/@sddkal/newton-square-root-method-in-python-270853e9185d
 # reference: https://www.w3schools.com/python/ref_func_round.asp
 # reference: https://www.geeksforgeeks.org/find-root-of-a-number-using-newtons-method/
+# reference: https://runestone.academy/runestone/books/published/thinkcspy/MoreAboutIteration/NewtonsMethod.html
 
 # first define function sqrt
 # the number value is defined later in the code
@@ -14,18 +15,33 @@
 # n is a postive number you want to find square root of
 # a is your guess that when squared will be close to equalling n 
 
-# define function: n is the number inputted by user, and the number iterations to run
+# define function: n is the number inputted by user and a count value
 # a is initially the number inputted by the user - initally this is used as the first guess
+# the code takes this number and halves it as a first approximate guess
+# the code also takes this number and performes Newton's Method using the approximate guess
+# if the two guesses do not match then the loop runs again using the latest approximate guess
 # each time an iteration is run, the result of the previous guess is used to make the answer for n more accurate
-def sqrt(n, iterations = 1000):
-    a = float(n)
-    for i in range(iterations):
-        n = 0.5 * (n + a / n)
-    return n
+# a counter tracks the number of iterations
+# once the guess is not able to be improved (when the two guess values match), the loop ends
+# the program prints the number of iterations run and the square root to 4 decimal places
 
+def sqrt(n, count):
+    a = float(n)
+    approx = 0.5 * a
+    better = 0.5 * (approx + n/approx)
+    while better != approx: 
+        approx = better
+        better = 0.5 * (approx + n/approx)
+        count = count + 1
+    print ("The number of iterations is: {}".format(count))
+    return approx
+    
 number = float(input("Please enter a number: "))
 
 #runs the function
-answer = (sqrt(number))
-# rounds the result to 1 decimal place
-print ("The estimated square root is: {}".format(round(answer,1)))
+answer = (sqrt(number, 1))
+
+# rounds the result to 4 decimal places
+print ("The estimated square root is: {}".format(round(answer,4)))
+
+
