5fbbde7c3ad58ca26cffebfbadb9d5dcd3d0f408
Andy Walker <g00398788@gmit.ie>
2021-03-30 09:59:48 +0100

    Correct previous code change line 27

--- weekday.py
+++ weekday.py
@@ -4,6 +4,7 @@
 # Author: Andy Walker
 
 # reference: https://pythontic.com/datetime/date/weekday accessed 23/2/21
+# reference: https://www.w3schools.com/python/python_datetime.asp accessed 30/3/21
 # reference: https://stackoverflow.com/questions/12382190/automatically-update-stored-value-of-datetime-datetime-now accessed 23/2/21
 
 # import Python's datetime module
@@ -24,7 +25,7 @@ todayAsString = days[today]
 
 # if the day of the week is one of the first 5 days in tuple print that it is a weekday.
 # if the day of the week is not one of the first 5 days then it prints that is is a weekend.
-if today <= 5:
+if today <= 4:
     print ("Today is {} which is a weekday.".format(todayAsString))
 else:
     print ("Yay! Today is {} which is the weekend!".format(todayAsString))
