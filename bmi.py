f559015f6c0179215ae207e155550fa116e84a62
Andy Walker <g00398788@gmit.ie>
2021-04-16 12:00:10 +0100

    Final Submission

--- bmi.py
+++ bmi.py
@@ -7,7 +7,7 @@
 height = int(input("Enter height in centimetres: "))
 weight = int(input("Enter weight in kilograms: "))
 
-# these lines are the outputs
+# these lines are the outputs 
 # BMI = kg/m2 where kg is a person's weight in kilograms and m2 is their height in metres squared
 # to calculate in centimetres, the height in metres must be divided by 100
 # the operator **2 is used to calculate to the power of 2
